2011ba2c8bbe33c0622b5be649ea5b6e7ad22cc9
PleahMaCaka <pleahmacaka@gmail.com>
2023-08-19 23:59:14 +0900

    feat: slide-menu button

---
+++ src/lib/components/menu/MenuButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let name: string
+    export let execute: Function
+    export let discirption: string
+</script>
+
+<button class="menu-btn">
+    {name}
+</button>
+
+<style>
+    .menu-btn {
+        background-color: #fff;
+        border: 1px solid #000;
+        border-radius: 5px;
+        padding: 5px 10px;
+        margin: 5px;
+        cursor: pointer;
+    }
+</style>
