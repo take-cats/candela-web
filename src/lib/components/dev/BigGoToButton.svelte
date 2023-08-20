b5d44f04462ad4f76d656c5977a8e3e3f9525004
PleahMaCaka <pleahmacaka@gmail.com>
2023-08-21 00:25:07 +0900

    feat: developer page

---
+++ src/lib/components/dev/BigGoToButton.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let name: string = "None"
+    export let goto: string = "/"
+</script>
+
+<a
+    class="big-goto-btn"
+    href={goto}
+>
+    <span class="big-goto-content">{name}</span>
+</a>
+
+<style>
+    .big-goto-content {
+        font-size: 1.3rem;
+    }
+
+    .big-goto-btn {
+        display: block;
+        width: 34%;
+        height: 48px;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+        background-color: #000;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 1.5rem;
+        font-weight: bold;
+        transition: all 0.3s;
+        line-height: 40px;
+    }
+
+    .big-goto-btn:hover {
+        background-color: rgba(100, 100, 100, 0.5);
+        color: #000;
+    }
+</style>
---
+++ src/routes/dev/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    import BigGoToButton from "$lib/components/dev/BigGoToButton.svelte";
+</script>
+
+<div id="bg-dev-page">
+    <h2 id="dev-title">
+        🕯️
+        ️ Dev👨‍💻's Page
+        🕯️
+    </h2>
+    <div id="big-btn-box">
+        <BigGoToButton
+            name="API Docs"
+            goto="/api/docs"
+        />
+        <BigGoToButton
+        />
+        <BigGoToButton/>
+        <BigGoToButton/>
+    </div>
+</div>
+
+<style>
+    #big-btn-box {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap: wrap;
+        width: 100%;
+        height: 20%;
+    }
+
+    #dev-title {
+        font-size: 2em;
+        font-weight: 700;
+        margin-bottom: 50px;
+        width: 100%;
+        padding: 0;
+        text-align: center;
+    }
+
+    #bg-dev-page {
+        width: 100vw;
+        height: 100vh;
+
+        color: white;
+        background: linear-gradient(45deg, #1D1D1D 75%, #2D2D2D 100%);
+
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+    }
+</style>
