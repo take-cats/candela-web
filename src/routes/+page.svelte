f42f72862d46234ef041e2bea16e71995a40802e
PleahMaCaka <pleahmacaka@gmail.com>
2023-08-23 10:09:08 +0900

    refactor: create `/app` route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,34 @@
 <script>
-    import CameraHolder from "$lib/components/camera/CameraHolder.svelte"
-    import TakePicBtn from "$lib/components/camera/TakePicBtn.svelte"
-    import SlideMenu from "$lib/components/menu/SlideMenu.svelte"
+    import BigGoToButton from "$lib/components/dev/BigGoToButton.svelte"
 </script>
 
-<CameraHolder/>
-<TakePicBtn/>
-<SlideMenu/>
+<div id="bg">
+    <BigGoToButton
+        goto="/dev"
+        name="Dev"/>
+    <BigGoToButton
+        goto="/app"
+        name="App"/>
+</div>
+
+<style>
+    #bg {
+        background-color: #000;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: .5;
+        z-index: -1;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+        padding-top: 45vh;
+    }
+</style>
